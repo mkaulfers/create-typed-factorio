a56fab383c20cef2ad31d01eb31f6490c0b31a3b
Matthew Kaulfers <42121989+mkaulfers@users.noreply.github.com>
2023-03-03 18:56:49 -0600

    Delete build directory

--- build/config.d.ts
+++
@@ -1,6 +0,0 @@
-export declare type Config = {
-    dirname: string;
-    projectName: string;
-    factorioVersion: string;
-};
-//# sourceMappingURL=config.d.ts.map
--- build/index.d.ts
+++
@@ -1,3 +0,0 @@
-import { Config } from "./config";
-export declare const create: (config: Config) => Promise<void>;
-//# sourceMappingURL=index.d.ts.map
